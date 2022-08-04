89f11cf91236f79b4be7e67a8b1ccc6b6a5aae21
Vasya564 <dudlavasil@gmail.com>
2022-08-04 13:20:49 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <div>
+            This react app is made without create-react-app 
+        </div>
+    );
+};
+
+export default App;
